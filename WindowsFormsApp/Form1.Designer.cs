6f701ad936622b22fddca8ea9472dfa9923de499
Артур Александрович Лукашов <lukashov.1300@mail.ru>
2021-06-21 01:46:42 +0300

    Revert "ver 1.0"
    This reverts commit fb50c3a62a70a700b63bef2dad8ee367bcfec53e.

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -30,92 +30,22 @@ namespace WindowsFormsApp
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.Choose_File = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Read_from_file = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // Choose_File
-            // 
-            this.Choose_File.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Choose_File.Location = new System.Drawing.Point(12, 44);
-            this.Choose_File.Name = "Choose_File";
-            this.Choose_File.Size = new System.Drawing.Size(362, 46);
-            this.Choose_File.TabIndex = 0;
-            this.Choose_File.Text = "Выбрать файл";
-            this.Choose_File.UseVisualStyleBackColor = true;
-            this.Choose_File.Click += new System.EventHandler(this.Choose_File_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Имя файла:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(101, 9);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(273, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // Read_from_file
-            // 
-            this.Read_from_file.Enabled = false;
-            this.Read_from_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Read_from_file.Location = new System.Drawing.Point(15, 105);
-            this.Read_from_file.Name = "Read_from_file";
-            this.Read_from_file.Size = new System.Drawing.Size(359, 55);
-            this.Read_from_file.TabIndex = 4;
-            this.Read_from_file.Text = "Прочитать данные из текстового файла";
-            this.Read_from_file.UseVisualStyleBackColor = true;
-            this.Read_from_file.Click += new System.EventHandler(this.Read_from_file_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.richTextBox1.Location = new System.Drawing.Point(381, 9);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(465, 312);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(858, 333);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.Read_from_file);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Choose_File);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Practical_Task";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Choose_File;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button Read_from_file;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
